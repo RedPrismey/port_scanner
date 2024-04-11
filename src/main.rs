0b077f5091227e77d4533f9dc40913783c771972
Prismey <prismey@murena.io>
2024-04-11 11:17:19 +0200

    Added comments to the code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use clap::Parser;
 
-/// Simple program to greet a person
+/* ---[Argument Structure]---
+*
+* Handling arguments with clap (see : https://docs.rs/clap/latest/clap/)
+* The arguments with no default value are considered required
+* */
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long)]
     ip: String,
+
+    #[arg(short, long)]
+    ports: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Hello {}!", args.ip);
+    println!("IP : {}", args.ip);
+    println!("ports : {}", args.ports);
 }
